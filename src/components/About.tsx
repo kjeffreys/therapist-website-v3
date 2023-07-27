f195568ccc84aff20e241fa2d57e84bfde35c942
kjeffreys <kjeffreyscs@gmail.com>
2023-07-27 11:50:43 -0400

    Created intial Services page will sample images and services covered

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './About.css';
 
-const About: React.FC = () => {
+const About: React.FC = () =>
+{
   return (
-    <section>
-      <h1>About</h1>
-      {/* Add your about page content here */}
-    </section>
+    <div className="about-container">
+      <div className="about-section">
+        <h2>About Me</h2>
+        <p>Hi, I’m Joanna Jeffreys. I’m a Licensed Mental Health Counselor (LMHC) and Licensed Professional Counselor (LPC) with over 5 years experience helping clients struggling with trauma, anxiety, and life transition.</p>
+      </div>
+      <div className="approach-section">
+        <h2>My Approach</h2>
+        <p>I want to join you in the highs and lows of your life. The great moments and the dark ones–they all belong in the therapy room. My style is warm, relaxed, and down-to-earth. You can expect compassion, understanding, and a focus on strengths and solutions, free of judgment or stigma. You bring the expertise on your own life, I’ll bring the clinical skills. Together, we can create radical change.</p>
+      </div>
+    </div>
   );
 };
 
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import './Services.css';
 
-const Services: React.FC = () => {
+const Services: React.FC = () =>
+{
+  const icImg = useMemo(() => require('../img/ic.jpg'), []);
+  const emdrImg = useMemo(() => require('../img/emdr.jpg'), []);
+  const ieImg = useMemo(() => require('../img/ie.jpg'), []);
+
   return (
-    <section>
+    <div>
       <h1>Services</h1>
-      {/* Add your services page content here */}
-    </section>
+      <div className="services-container">
+        <div className="service">
+          <h2>Individual Counseling</h2>
+          <div className="img-wrapper">
+            <img src={icImg} alt="Individual Counseling" />
+          </div>
+          <p>Individual talk therapy might be what first comes to your mind when you think of therapy. You talk, I listen and offer support, feedback, and interventions. Talk therapy is an opportunity to get to know yourself better–your story, your needs, strengths, and areas for growth. I tend to focus interventions on self-compassion, boundaries, distress tolerance, and mindfulness.</p>
+        </div>
+        <div className="service">
+          <h2>EMDR</h2>
+          <div className="img-wrapper">
+            <img src={emdrImg} alt="EMDR" />
+          </div>
+          <p>You deserve to heal from trauma. Using the EMDR framework, we will identify trauma-related symptoms, desensitize trauma reactions through reprocessing traumatic memories, and empower you to respond differently to trauma triggers in the future. By engaging with your trauma in a safe, supportive, structured environment, you can not only heal old wounds, but find move forward into the thriving part of your life.</p>
+        </div>
+        <div className="service">
+          <h2>Intuitive Eating</h2>
+          <div className="img-wrapper">
+            <img src={ieImg} alt="Intuitive Eating" />
+          </div>
+          <p>I believe in food freedom. Intuitive Eating is all about honoring and accepting your body. Over time, we learn toxic messages around diet culture and body shaming, which can eventually present as self-consciousness or disordered eating. I am a certified IE coach and can help you cultivate a peaceful relationship with your mind, body, and food!</p>
+        </div>
+        <div className="service">
+          <h2>Supervision</h2>
+          <div className="img-wrapper">
+            <img src={icImg} alt="Supervision" />
+          </div>
+          <p>Supervision is a valuable tool in any clinician's toolbox. In addition to occasionally feeling “stuck” clinically, our own “stuff”--countertransference, triggers, and vicarious trauma, can come into play in the therapy room. Supervision provides an outlet to release that tension, and gain valuable insight around case conceptualization and personal process. Please contact me to discuss availability and rates!</p>
+        </div>
+        <div className="service">
+          <h2>Telehealth</h2>
+          <div className="img-wrapper">
+            <img src={icImg} alt="Telehealth" />
+          </div>
+          <p>I primarily offer telehealth services via secure Zoom video calls. If you are located in NY, sessions will exclusively be held remotely. Telehealth offers many benefits, including ability to meet in the safe, comfortable place of your choice, flexible scheduling</p>
+          <p><b>NOTE:</b> Due to licensure laws, you <em>MUST</em> be located in either New York or Virginia (my states of licensure) at the time of session for us to meet. If you are not located in one of these states, the session cannot legally be held.</p>
+        </div>
+        <div className="service">
+          <h2>In-person</h2>
+          <div className="img-wrapper">
+            <img src={icImg} alt="In-person" />
+          </div>
+          <p>I offer in-person services for clients located in the Lynchburg, VA area. Please send me an email to inquire about in-office availability!</p>
+        </div>
+        <div className="service">
+          <h2>Payment</h2>
+          <div className="img-wrapper">
+            <img src={icImg} alt="Payment" />
+          </div>
+          <h3>I accept the following insurance:</h3>
+          <ul>
+            <li>Aetna</li>
+            <li>BlueCross BlueShield</li>
+            <li>Cigna/Evernorth</li>
+            <li>Oscar Health</li>
+            <li>Oxford</li>
+            <li>United Healthcare</li>
+            <li>Sliding scale for self-pay clients available upon request</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
